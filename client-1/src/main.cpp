338830a539012f2d9694bf4db192d9c56bccad9a
NirajMalokar21 <nirajmalokar@gmail.com>
2026-02-05 20:59:03 -0500

    Set up mqtt communication

--- client-1/src/main.cpp
+++ client-1/src/main.cpp
@@ -1,18 +1,91 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "";
+const char* password = "";
+
+const char* mqtt_server = "192.168.0.97";  // laptop IP
+const int mqtt_port = 4242;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 // put function declarations here:
-int myFunction(int, int);
+void callback(char* topic, byte* payload, unsigned int length);
+void setup_wifi();
+void reconnect();
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3); // Test9ig theory
+  Serial.begin(115200);
+  setup_wifi();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();   // keeps MQTT alive
+
+  // Example: publish every 5 seconds
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 5000) {
+    lastMsg = millis();
+    client.publish("studybuddy/A/state",
+                   "{ \"state\": \"THIS IS NIRAJ\" }");
+  }
 }
 
 // put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
 }
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+
+    if (client.connect("ESP32_A")) {
+      Serial.println("connected");
+
+      client.subscribe("studybuddy/A/buddy");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" retrying in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
